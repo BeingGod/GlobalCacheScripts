1444f43b7aa764dadaa7098b5ecdba683628b05d
beinggod <beinggod@foxmail.com>
2023-03-19 20:00:55 +0800

    [feature] add ceph check script

---
+++ deploy/ceph/ceph_check.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#------------------------------------------------------------------------------------
+# Description: 检查Ceph部署情况
+# Author: beinggod
+# Create: 2023-03-19
+#-----------------------------------------------------------------------------------
+set -e
+SCRIPT_HOME=$(cd $(dirname $0)/; pwd)
+LOG_FILE=$SCRIPT_HOME/../../log/globalcache_script.log
+source $SCRIPT_HOME/../../common/log.sh
+
+function main()
+{
+    ceph -s
+    if [[ $? -ne 0 ]]; then
+        globalcache_log "[$BASH_SOURCE,$LINENO,$FUNCNAME]:ceph is not deployed!" ERROR && return 1
+    fi
+}
+main
