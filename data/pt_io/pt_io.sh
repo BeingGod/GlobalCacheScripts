c8ca044b78b4e387c0e0d6833ab7e0196479806d
beinggod <beinggod@foxmail.com>
2023-03-19 19:37:14 +0800

    [fix] fix script log path

--- data/pt_io/pt_io.sh
+++ data/pt_io/pt_io.sh
@@ -6,7 +6,7 @@
 #-----------------------------------------------------------------------------------
 set -e
 SCRIPT_HOME=$(cd $(dirname $0)/; pwd)
-LOG_FILE=/var/log/globalcache_script.log
+LOG_FILE=$SCRIPT_HOME/../../log/globalcache_script.log
 source $SCRIPT_HOME/../../common/log.sh
 
 # @brief 读取PT_IO状态信息
