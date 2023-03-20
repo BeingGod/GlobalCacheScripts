da9bcdeca4c97d710de878e7f5ee3a2c171774ed
beinggod <beinggod@foxmail.com>
2023-03-20 20:22:45 +0800

    [fix] fix query disk info script

--- data/disk/disk.sh
+++ data/disk/disk.sh
@@ -6,14 +6,23 @@
 #-----------------------------------------------------------------------------------
 set -e
 SCRIPT_HOME=$(cd $(dirname $0)/; pwd)
-LOG_FILE=$SCRIPT_HOME/../../log/globalcache_script.log
+LOG_FILE=/var/log/globalcache_script.log
 source $SCRIPT_HOME/../../common/log.sh
 
-# @brief 读取磁盘信息
+# 读取磁盘信息
 function main()
 {
     local sys_disk_name=$(lsblk | grep "/boot/efi" | grep -oe "sd[a-z]") # 获取系统盘所在磁盘名称
     local all_disk_name=$(lsblk | grep "^[a-z].*" | cut -d ' ' -f 1)
-    echo ${all_disk_name//"${sys_disk_name}"/} # 移除系统盘
+    local use_disk_name=$(echo $all_disk_name | sed "s/$sys_disk_name//")
+
+    OLD_IFS="$IFS"
+    IFS=" "
+    array=($use_disk_name)
+    IFS="$OLD_IFS"
+    for var in ${array[@]}
+    do
+       lsblk -d -o name,rota | grep $var
+    done
 }
 main
