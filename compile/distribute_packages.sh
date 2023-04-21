0325952f96837dacef24daf46d27ecaae0b00442
BeingGod <beinggod@foxmail.com>
2023-04-21 14:18:44 +0800

    fix: distribute packages script bug

--- compile/distribute_packages.sh
+++ compile/distribute_packages.sh
@@ -23,8 +23,7 @@ function configure_pdsh_group()
   cat /home/hostnamelist.txt | grep -E -oe "ceph1" > /etc/dsh/group/ceph1                                  # ceph1
   cat /home/hostnamelist.txt | grep -E -oe "ceph[0-9]*" > /etc/dsh/group/ceph                              # all ceph
   cat /home/hostnamelist.txt | grep -E -oe "client[0-9]*" > /etc/dsh/group/client                          # all client
-  cat /home/hostnamelist.txt | grep -E -oe "(ceph[0-9]*)|(client[0-9]*)" > /etc/dsh/group/all              # all 
-  cat /home/hostnamelist.txt | grep -E -oe "(ceph[2-9]{1}[0-9]*)|(client[0-9]*)" > /etc/dsh/group/ex_ceph1 # exclude ceph1
+  cat /home/hostnamelist.txt | grep -E -oe "(ceph[0-9]*)|(client[0-9]*)" > /etc/dsh/group/all              # all
 
   globalcache_log "------------configure pdsh group end------------" WARN
 }
@@ -35,68 +34,68 @@ function distribute()
 
     cd /home
     if [[ -f "/home/OpenJDK8U-jdk_aarch64_linux_hotspot_jdk8u282-b08.tar.gz" ]];then
-        pdcp -g ex_ceph1 "/home/OpenJDK8U-jdk_aarch64_linux_hotspot_jdk8u282-b08.tar.gz" "/home/OpenJDK8U-jdk_aarch64_linux_hotspot_jdk8u282-b08.tar.gz"
+        pdcp -g client,ceph -X ceph1 "/home/OpenJDK8U-jdk_aarch64_linux_hotspot_jdk8u282-b08.tar.gz" "/home"
     else
         globalcache_log "[$BASH_SOURCE,$LINENO,$FUNCNAME]:OpenJDK8U-jdk_aarch64_linux_hotspot_jdk8u282-b08.tar.gz not exist!" ERROR && return 1
     fi
 
     if [[ -f "/home/apache-maven-3.6.3-bin.tar.gz" ]]; then
-        pdcp -g ex_ceph1 "/home/apache-maven-3.6.3-bin.tar.gz" "/home/apache-maven-3.6.3-bin.tar.gz"
+        pdcp -g client,ceph -X ceph1 "/home/apache-maven-3.6.3-bin.tar.gz" "/home"
     else
         globalcache_log "[$BASH_SOURCE,$LINENO,$FUNCNAME]:apache-maven-3.6.3-bin.tar.gz is not exist!" ERROR && return 1
     fi
 
-    if [[ -f "/home/apache-zookeeper-3.6.3-bin.tar.gz" ]]; then
-        pdcp -g ceph "/home/apache-zookeeper-3.6.3-bin.tar.gz" "/home/apache-zookeeper-3.6.3-bin.tar.gz"
+    if [[ -f "/home/apache-zookeeper-3.6.3.tar.gz" ]]; then
+        pdcp -g ceph -X ceph1 "/home/apache-zookeeper-3.6.3.tar.gz" "/home"
     else
-        globalcache_log "[$BASH_SOURCE,$LINENO,$FUNCNAME]:apache-zookeeper-3.6.3-bin.tar.gz is not exist!" ERROR && return 1
+        globalcache_log "[$BASH_SOURCE,$LINENO,$FUNCNAME]:apache-zookeeper-3.6.3.tar.gz is not exist!" ERROR && return 1
     fi
 
     if [[ -f "/home/fio-3.26.tar.gz" ]]; then
-        pdcp -g client "/home/fio-3.26.tar.gz" "/home/fio-3.26.tar.gz"
+        pdcp -g client "/home/fio-3.26.tar.gz" "/home"
     else
         globalcache_log "[$BASH_SOURCE,$LINENO,$FUNCNAME]:fio-3.26.tar.gz is not exist!" ERROR && return 1
     fi
 
     if [[ -f "/home/boostkit-globalcache-release-1.1.0.oe1.aarch64.rpm" ]]; then
-        pdcp -g ceph "/home/boostkit-globalcache-release-1.1.0.oe1.aarch64.rpm" "/home/boostkit-globalcache-release-1.1.0.oe1.aarch64.rpm"
+        pdcp -g ceph -X ceph1 "/home/boostkit-globalcache-release-1.1.0.oe1.aarch64.rpm" "/home"
     else
         globalcache_log "[$BASH_SOURCE,$LINENO,$FUNCNAME]:boostkit-globalcache-release-1.1.0.oe1.aarch64.rpm is not exist!" ERROR && return 1
     fi
 
     if [[ -f "/home/boostkit-globalcache-ceph-adaptor-release-1.1.0.oe1.aarch64.rpm" ]]; then
-        pdcp -g client "/home/boostkit-globalcache-ceph-adaptor-release-1.1.0.oe1.aarch64.rpm" "/home/boostkit-globalcache-ceph-adaptor-release-1.1.0.oe1.aarch64.rpm"
+        pdcp -g client "/home/boostkit-globalcache-ceph-adaptor-release-1.1.0.oe1.aarch64.rpm" "/home"
     else
         globalcache_log "[$BASH_SOURCE,$LINENO,$FUNCNAME]:boostkit-globalcache-ceph-adaptor-release-1.1.0.oe1.aarch64.rpm is not exist!" ERROR && return 1
     fi
 
     if [[ -f "/home/boostkit-zk-secure.tar.gz" ]]; then
-        pdcp -g ceph "/home/boostkit-zk-secure.tar.gz" "/home/boostkit-zk-secure.tar.gz"
+        pdcp -g ceph -X ceph1 "/home/boostkit-zk-secure.tar.gz" "/home"
     else
         globalcache_log "[$BASH_SOURCE,$LINENO,$FUNCNAME]:boostkit-zk-secure.tar.gz is not exist!" ERROR && return 1
     fi
 
-    pdsh -R ssh -g ex_ceph1 "mkdir -p /home/apache-zookeeper-3.6.3/zookeeper-client/zookeeper-client-c/target/c/"
+    pdsh -R ssh -g client,ceph -X ceph1 "mkdir -p /home/apache-zookeeper-3.6.3/zookeeper-client/zookeeper-client-c/target/c/"
     if [[ -d "/home/apache-zookeeper-3.6.3/zookeeper-client/zookeeper-client-c/target/c/" ]]; then
-        pdcp -r -g ex_ceph1 "/home/apache-zookeeper-3.6.3/zookeeper-client/zookeeper-client-c/target/c/" "/home/apache-zookeeper-3.6.3/zookeeper-client/zookeeper-client-c/target/c/" 
+        pdcp -r -g client,ceph -X ceph1 "/home/apache-zookeeper-3.6.3/zookeeper-client/zookeeper-client-c/target/c/" "/home/apache-zookeeper-3.6.3/zookeeper-client/zookeeper-client-c/target/c/"
     else
         globalcache_log "[$BASH_SOURCE,$LINENO,$FUNCNAME]:/home/apache-zookeeper-3.6.3/zookeeper-client/zookeeper-client-c/target/c/ is not exist!" ERROR && return 1
     fi
 
     if [[ -f "/home/server/adaptorlib/ceph-global-cache-adaptor/build/lib/libproxy.so" ]]; then
-        pdcp -g client "/home/server/adaptorlib/ceph-global-cache-adaptor/build/lib/libproxy.so" "/home/libproxy.so"
+        pdcp -g client "/home/server/adaptorlib/ceph-global-cache-adaptor/build/lib/libproxy.so" "/home"
     else
         globalcache_log "[$BASH_SOURCE,$LINENO,$FUNCNAME]:libproxy.so is not exist!" ERROR && return 1
     fi
 
     if [[ -f "/home/cephlib-release-oe1.tar.gz" ]]; then
-        pdcp -g ceph "/home/cephlib-release-oe1.tar.gz" "/home/cephlib-release-oe1.tar.gz"
+        pdcp -g ceph -X ceph1 "/home/cephlib-release-oe1.tar.gz" "/home/cephlib-release-oe1.tar.gz"
     else
         globalcache_log "[$BASH_SOURCE,$LINENO,$FUNCNAME]:cephlib-release-oe1.tar.gz is not exist!" ERROR && return 1
     fi
 
     if [[ -f "/home/globalcache-adaptorlib-release-oe1.tar.gz" ]]; then
-        pdcp -g ceph "/home/cephlib-release-oe1.tar.gz" "/home/cephlib-release-oe1.tar.gz"
+        pdcp -g ceph -X ceph1 "/home/cephlib-release-oe1.tar.gz" "/home"
     else
         globalcache_log "[$BASH_SOURCE,$LINENO,$FUNCNAME]:globalcache-adaptorlib-release-oe1.tar.gz is not exist!" ERROR && return 1
     fi
