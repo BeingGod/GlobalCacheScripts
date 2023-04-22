a938ee5a1504767b775d4276da52cd23d4ce4206
BeingGod <beinggod@foxmail.com>
2023-04-22 10:56:22 +0800

    fix: add client RPM check on server

--- compile/compile_check_server.sh
+++ compile/compile_check_server.sh
@@ -12,12 +12,14 @@ source $SCRIPT_HOME/../common/log.sh
 source $SCRIPT_HOME/compile_liboath.sh # 引入compile_liboath.sh脚本
 source $SCRIPT_HOME/compile_zookeeper.sh # 引入compile_zookeeper.sh脚本
 source $SCRIPT_HOME/compile_server.sh # 引入compile_server.sh脚本
+source $SCRIPT_HOME/compile_client.sh # 引入compile_client.sh脚本
 
 function main()
 {
 cd /home
     compile_liboath_check # 检查liboath
     compile_zookeeper_check # 检查zookeeper
+    compile_client_check # 检查client
     compile_server_check # 检查server
 }
 main
