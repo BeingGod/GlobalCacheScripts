9d2df0e5c18a8d9096433673cc1d0e0076a224bf
BeingGod <beinggod@foxmail.com>
2023-04-26 13:31:38 +0800

    fix: ceph check script

--- deploy/ceph/ceph_check.sh
+++ deploy/ceph/ceph_check.sh
@@ -5,14 +5,16 @@
 # Create: 2023-03-19
 #-----------------------------------------------------------------------------------
 set -x
+set -x
 SCRIPT_HOME=$(cd $(dirname $0)/; pwd)
 LOG_FILE=$SCRIPT_HOME/../../log/globalcache_script.log
 source $SCRIPT_HOME/../../common/log.sh
 
 function main()
 {
-    ceph -s
-    if [[ $? -ne 0 ]]; then
+    if ceph -s > /dev/null 2>&1; then
+        ceph -s
+    else
         globalcache_log "[$BASH_SOURCE,$LINENO,$FUNCNAME]:ceph is not deployed!" ERROR && return 1
     fi
 }
