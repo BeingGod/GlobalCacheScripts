009b6772cd3c0f906d3b5e0835b9bb7a568fea91
BeingGod <beinggod@foxmail.com>
2023-05-09 11:19:39 +0800

    feat: add query hostname script

---
+++ data/hostname/hostname.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#------------------------------------------------------------------------------------
+# Description: 节点名称 
+# Author: beinggod
+# Create: 2023-4-30
+#-----------------------------------------------------------------------------------
+
+SCRIPT_HOME=$(cd $(dirname $0)/; pwd)
+LOG_FILE=$SCRIPT_HOME/../../log/globalcache_script.log
+source $SCRIPT_HOME/../../common/log.sh
+
+#  读取内存信息
+function main()
+{
+    echo "$(hostname)"
+}
+main
