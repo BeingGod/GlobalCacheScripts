b80ff0116f38535188d8e016ac35d113e352a33a
BeingGod <beinggod@foxmail.com>
2023-06-08 10:45:12 +0800

    feat: fix adaptor download

--- compile/compile_server.sh
+++ compile/compile_server.sh
@@ -38,12 +38,15 @@ function server_prepare()
     if [ -d "server/adaptorlib/ceph-global-cache-adaptor" ]; then
         globalcache_log "ceph-global-cache-adaptor does not need to be prepared." INFO
     else
-        cd server/adaptorlib
-        git clone https://github.com/666syh/ceph-global-cache-adaptor.git
-        [[ $? -ne 0 ]] && globalcache_log "[$BASH_SOURCE,$LINENO,$FUNCNAME]:git clone ceph-global-cache-adaptor failed!" ERROR && return 1
-        cd ceph-global-cache-adaptor 
-        git checkout T14
+        tar -xzvf /home/ceph-global-cache-adaptor-T14.tar.gz
+        cp -r /home/ceph-global-cache-adaptor-T14 server/adaptor/lib
         cd ../../../
+        # cd server/adaptorlib
+        # git clone https://github.com/666syh/ceph-global-cache-adaptor.git
+        # [[ $? -ne 0 ]] && globalcache_log "[$BASH_SOURCE,$LINENO,$FUNCNAME]:git clone ceph-global-cache-adaptor failed!" ERROR && return 1
+        # cd ceph-global-cache-adaptor 
+        # git checkout T14
+        # cd ../../../
     fi
 
     globalcache_log "------------server prepare end------------" WARN
